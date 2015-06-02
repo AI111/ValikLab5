8ebc19cec4662182ac814da0e4a62cf19b820a50
sasha <sashaandreev7@gmail.com>
2015-06-02 23:26:17 +0300

    add service implimentation and tests

--- src/main/java/com/valik/Main.java
+++ src/main/java/com/valik/Main.java
@@ -4,9 +4,6 @@ import com.valik.domain.BookRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Created by валик on 02.06.2015.
- */
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
--- src/main/java/com/valik/service/ApplicationServiceImpl.java
+++ src/main/java/com/valik/service/ApplicationServiceImpl.java
@@ -6,29 +6,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Created by валик on 02.06.2015.
- */
+
 public class ApplicationServiceImpl implements AplicationService {
     @Autowired
     BookRepository bookRepository;
+
+    public ApplicationServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     @Transactional
     public int removeBooksWithKNames() {
         List<Book> list = bookRepository.getAllBooks();
-        Iterator<Book> iterable =list.listIterator();
-        while(iterable.hasNext()){    
-
+        int count = 0;
+        for(Book book:list){
+            if(book.getName().startsWith("K")){
+                bookRepository.removeBook(book);
+                count++;
+            }
         }
-        return 0;
-    }
 
+        return count;
+    }
     @Override
     @Transactional
     public int removeBoksIdNotFibanachi() {
-        return 0;
+        List<Book> list = bookRepository.getAllBooks();
+        LinkedList<Integer> fibanachi = new LinkedList<>();
+        fibanachi.add(0);
+        fibanachi.add(1);
+        int count = 0;
+        for(Book book:list){
+            if(book.getId()>1&&book.getId()==fibanachi.getLast()+fibanachi.get(fibanachi.size()-2)){
+                fibanachi.add(book.getId());
+            }else{
+                bookRepository.removeBook(book);
+                count++;
+            }
+        }
+        return count;
     }
 }
